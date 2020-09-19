5d4d52a4e4b119ffa5db182efab704def79664f7
sandeep paritala <sandeepkumarparitala@gmail.com>
2020-09-19 17:41:08 +0530

    .

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, request, render_template
+from flask import Flask, escape, request, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -24,6 +24,71 @@ def hello():
         "ventureCapital": True,
         "email":"abcd@abc.com",
         "readinessValue": "40",
+        "treeData": [
+                {
+                  "id": "A",
+                  "name": "Apples",
+                },
+                {
+                  "id": "B",
+                  "name": "Bananas",
+                },
+                {
+                  "id": "O",
+                  "name": "Oranges",
+                },
+                {
+                  "id": "K",
+                  "name": "Anne",
+                  "parent": "A",
+                  "value": 5,
+                },
+                {
+                  "name": "Rick",
+                  "parent": "K",
+                  "value": 3,
+                },
+                {
+                  "name": "Peter",
+                  "parent": "A",
+                  "value": 4,
+                },
+                {
+                  "name": "Anne",
+                  "parent": "B",
+                  "value": 4,
+                },
+                {
+                  "name": "Rick",
+                  "parent": "B",
+                  "value": 10,
+                },
+                {
+                  "name": "Peter",
+                  "parent": "B",
+                  "value": 1,
+                },
+                {
+                  "name": "Anne",
+                  "parent": "O",
+                  "value": 1,
+                },
+                {
+                  "name": "Rick",
+                  "parent": "O",
+                  "value": 3,
+                },
+                {
+                  "name": "Peter",
+                  "parent": "O",
+                  "value": 3,
+                },
+                {
+                  "name": "Susanne",
+                  "parent": "Kiwi",
+                  "value": 2,
+                },
+              ],
         "about": "18+ years of experience in business development and Mergers and Acquisitions, completing over $70 billion in high value growth-oriented transactions, in fields ranging from medical devices and pharmaceuticals to semiconductors and software. In his most recent role as VP, Business Development, he added many platforms and technologies to Ethicon's portfolio, including those in digital surgery (3Di, Auris investment and collaboration), GERD (Torax), Interventional Oncology (NeuWave, Auris collaboration), Wound Closure (barbed sutures) and Energy (Megadyne)"
     }
     return render_template('index.html', data=data)
